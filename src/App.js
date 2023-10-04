a5b0d5c62fcc4e20259a2cda113dfec36027e80d
Isabella Cardozo <isabella.cardozom@uqvirtual.edu.co>
2023-10-03 22:56:31 -0500

    Pagina base de iniciar sesion

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 /*
-Cuando utilizas React.lazy para cargar un
+React.lazy es para cargar un
 componente dinámicamente, ese
 componente y sus estilos se cargan de forma independiente y se
 mantienen aislados del resto de la aplicación*/
