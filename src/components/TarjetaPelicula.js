cf81100e39748472fb20351d1960b6be2209dfcc
anrumo <anrumo232@gmail.com>
2023-10-07 22:06:20 -0500

    tarjetitas a punto de caramelo

--- src/components/TarjetaPelicula.js
+++ src/components/TarjetaPelicula.js
@@ -1,23 +1,26 @@
 import React from "react";
 import '../styles/TarjetaPeliculaStyle.css';
+import {Link} from "react-router-dom";
 function TarjetaPelicula (prop){
 
     const peli = prop.movie;
     const horariosCortos = ["01:20", "04:20", "07:20", "10:20"];
     const horariosLargos = ["01:15", "06:15"];
 
+    console.log(peli.duracionMinutos);
+
     return(
         <div className="container-tarjeta">
             <article className="tarjeta">
-                <img src="https://hips.hearstapps.com/hmg-prod/images/gabz-godfather-1646152137.png" className="poster"/>
-                <h2 key={peli.id} className="nombre-peli">{peli.nombre}</h2>
+                <Link to="/" className="link-poster"><img src="https://hips.hearstapps.com/hmg-prod/images/gabz-godfather-1646152137.png" className="poster"/></Link>
+                <Link to="/" className="link-titulo"><h2 key={peli.id} className="nombre-peli">{peli.nombre}</h2></Link>
                 <h3 className="horarios-title">Horarios:</h3>
                 <ul className="horarios-list">
                     {
-                        peli.duracion < 140 ? horariosCortos.map(horario =>
-                                <li className="horario-item">{horario}</li>)
+                        peli.duracionMinutos < 140 ? horariosCortos.map(horario =>
+                                <Link to="/" className="link-horario"><li className="horario-item">{horario}</li></Link>)
                             : horariosLargos.map(horario =>
-                                <li className="horario-item">{horario}</li>)
+                                <Link to="/" className="link-horario"><li className="horario-item">{horario}</li></Link>)
                     }
                 </ul>
             </article>
